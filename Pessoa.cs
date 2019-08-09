166037753f3f0bae298c61abfb5489c3785b308e
“Lauane <laupinhosilva@gmail.com>
2019-08-09 08:54:55 -0300

    primeiro commit

---
+++ Pessoa.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovoProjeto
+{
+    class Pessoa
+    {
+        private string nome;
+        private string cpf;
+        private int idade; // Bem Novinha
+        private bool solteiro; //Muito Solteira 
+
+
+        public string Nome { get; set; }
+        public void Login()
+
+        {
+
+        }
+        public void AlterarCadastro()
+        {
+
+        }
+        public void ConsultarNotas()
+        {
+
+        }
+
+        public string LerNome()
+        {
+            if (nome == null )
+            {
+                Console.WriteLine(" O nome ainda não foi cadastrado.");
+
+            }
+        return nome;
+        }
+        public void GravarNome(string valor)
+        {
+        if (valor.Lenght > 10)
+        {
+            Console.WriteLine("O nome deve conter no máximo 10 caracteres.");
+        }
+        else
+        {
+        nome  = valor; 
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovoProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Hello World!);
+           Pessoa p1 =  new Pessoa();
+            p1.nome = "Lauane";
+            p1.Login();
+
+
+            Console.WriteLine(p1.LerNome());
+            p1.Nome = "Lauane";
+            Console.WriteLine(p1.Nome);
+
+
+
+
+
+
+        }
+    }
+}
